f1ce2d7aa039779e25751fdf510ebed066c3d8fe
Aaryan1604 <aryanpanthi3@gmail.com>
2023-11-21 22:26:01 -0500

    starting the input check i.e step 1

--- fibonachi.s
+++ fibonachi.s
@@ -1,4 +1,13 @@
-.data
-prompt:         .asciiz "Enter an integer (N >= 25): "
-error_display:      .asciiz "Illegal Number!\n"
+.data    #initializing static variables
+prompt:             .asciiz "Enter an integer (N >= 25): "
+error_display:      .asciiz "Illegal Number! Please Write valid numbers i.e (N >= 25) \n"
 fib_display:        .asciiz "Fibonacci Sequence:\n"
+
+
+.text  #writing executable codes 
+
+main:
+    #loop continue until user inputs valid number
+    j input_loop   #using jump instuciton to go to label input_loop
+
+input_loop 
